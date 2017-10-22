daa978c9dd3b5dff5f11641af90e395d035f7756
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2017-10-22 01:50:12 -0200

    Add docs for cell class

--- lib/minesweeper/cell.rb
+++ lib/minesweeper/cell.rb
@@ -1,6 +1,20 @@
+require 'contracts'
+
 module Minesweeper
+
+  # Objects of this class store the state of cells from minesweeper board.
   class Cell
+    include Contracts::Core
+    include Contracts::Builtin
+    C = Contracts
+
     attr_accessor :neighbor_bombs
+
+    # @param bomb cell has a bomb
+    # @param flag cell is flagged
+    # @param open cell is open
+    # @param neighbor_bombs amount of neighbor cells with mines
+    Contract C::Bool, C::Bool, C::Bool, C::Nat => C::Any
     def initialize(bomb, flag=false, open=false, neighbor_bombs=0)
       @open = open
       @flag = flag
@@ -8,18 +22,27 @@ module Minesweeper
       @neighbor_bombs = neighbor_bombs
     end
     
+    # Cell has a bomb
+    Contract C::None => C::Bool
     def bomb?
       @bomb
     end
     
+    # Cell is open
+    Contract C::None => C::Bool
     def open?
       @open
     end
     
+    # Cell is flagged
+    Contract C::None => C::Bool
     def flag?
       @flag
     end
     
+    # Tries to open the cell. To have success the cell must be previously closed and not be flagged.
+    # @return true if the cell could be open.
+    Contract C::None => C::Bool
     def play
       if ((not @open) and (not @flag)) then
         @open = true
@@ -28,6 +51,9 @@ module Minesweeper
       end
     end
     
+    # Toggles the flag status. An open cell cannot be flagged.
+    # @return true if flag status could be toggled. False otherwise.
+    Contract C::None => C::Bool
     def toggle_flag
       if not @open then
         @flag = (not @flag)
